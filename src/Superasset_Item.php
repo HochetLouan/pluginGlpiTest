cde66bcfc06d63ee88b4845dc69eddf8c98946d5
Louan <hochetlouan7@gmail.com>
2026-04-20 09:54:57 +0200

    fix(tabs)

--- src/Superasset_Item.php
+++ src/Superasset_Item.php
@@ -2,12 +2,18 @@
 
 namespace GlpiPlugin\Test;
 
-use CommonDBTM;
+use CommonDBRelation;
 use CommonGLPI;
+use Computer;
 use Glpi\Application\View\TemplateRenderer;
+use GlpiPlugin\Test\Superasset;
 
-class Superasset_Item extends CommonDBTM
+class Superasset_Item extends CommonDBRelation
 {
+
+    static public $itemtype_primary = 'plugin_test_superassets_id';
+    static public $itemtype_secondary = 'itemtype';
+    static public $items_id_secondary = 'items_id';
     /**
      * Tabs title
      */
@@ -15,26 +21,29 @@ class Superasset_Item extends CommonDBTM
     {
         switch ($item->getType()) {
             case Superasset::class:
-                $nb = countElementsInTable(self::getTable(),
-                    [
-                        'plugin_test_superassets_id' => $item->getID()
-                    ]
-                );
-                return self::createTabEntry(self::getTypeName($nb), $nb);
+                $nb = countElementsInTable(self::getTable(), [
+                    'plugin_test_superassets_id' => $item->getID()
+                ]);
+                return self::createTabEntry(__('Associated Items', 'test'), $nb);
+
+            case Computer::class:
+                $nb = countElementsInTable(self::getTable(), [
+                    'itemtype' => Computer::class,
+                    'items_id' => $item->getID()
+                ]);
+                return self::createTabEntry(__('Superassets', 'test'), $nb);
         }
         return '';
     }
 
-    /**
-     * Display tabs content
-     */
+    
     static function displayTabContentForItem(CommonGLPI $item, $tabnum = 1, $withtemplate = 0)
     {
-        switch ($item->getType()) {
-            case Superasset::class:
-                return self::showForSuperasset($item, $withtemplate);
+        if ($item->getType() == Superasset::class) {
+            self::showForSuperasset($item);
+        } else if ($item->getType() == Computer::class) {
+            self::showForComputer($item);
         }
-
         return true;
     }
 
@@ -45,6 +54,11 @@ class Superasset_Item extends CommonDBTM
     {
         TemplateRenderer::getInstance()->display('@test/superasset_item.html.twig', [
             'superasset' => $superasset,
+            'items'      => getAllDataFromTable(self::getTable(), ['plugin_test_superassets_id' => $superasset->getID()])
         ]);
+    }
+
+    static function showForComputer(Computer $computer) {
+        echo "Contenu de l'onglet sur le PC"; 
     }
 }
